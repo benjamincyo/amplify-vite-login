0675352c1e13ede00f4c3d57dfca548150c12aa9
benjamincyo <benjamincyo@gmail.com>
2024-05-28 20:50:01 -0600

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,9 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: false,
+    phoneNumber: true,
   },
+  signUpAttributes: ['phone_number'],
+  verificationMechanisms: ['SMS'],
 });
